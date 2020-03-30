be46582ccd79f98ba967311896c9aec4a8eb143c
Vivek <Vivek@DESKTOP-M12KHDD>
2020-03-30 12:40:00 +0100

    It Holds the data of Most Moblie search

---
+++ Moblie Managment/src/com/data/MoblieQueue.java
@@ -0,0 +1,29 @@
+package com.data;
+
+public class MoblieQueue {
+
+	private String name;
+	private int count;
+
+	public MoblieQueue(String name, int count) {
+		this.name = name;
+		this.count = count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
